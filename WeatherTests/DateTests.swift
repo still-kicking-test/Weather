2e37c773789d596e4225f5e1acf8522a27236a5d
Jonathan Saville <jonathan.saville@mac.com>
2024-03-13 12:40:42 +0000

    Fixed Date tests

--- WeatherTests/DateTests.swift
+++ WeatherTests/DateTests.swift
@@ -14,30 +14,32 @@ final class DateTests: XCTestCase {
     let honoluluSecondsFromGMT: Int = DateTests.secondsIn(hours: -10)  // Hawaii does not use daylight saving time
     let londonSecondsFromGMT: Int = DateTests.secondsIn(hours: 1)      // London does use daylight saving time (of course) - and it is included in the timezoneOffset sent by the API
 
-    func testShortDayOfWeek() {
+    func testDayOfWeek() {
         continueAfterFailure = false
-        let dateOct_25_08AM_GMT = Date(timeIntervalSince1970: 1698220800)
-        XCTAssertEqual(dateOct_25_08AM_GMT.shortDayOfWeek(londonSecondsFromGMT), "Wed")
-        XCTAssertEqual(dateOct_25_08AM_GMT.shortDayOfWeek(tokyoSecondsFromGMT), "Wed")
-        XCTAssertEqual(dateOct_25_08AM_GMT.shortDayOfWeek(honoluluSecondsFromGMT), "Tue")
+        let oct_25_08AM_GMT = Date(timeIntervalSince1970: 1698220800)
+        XCTAssertEqual(oct_25_08AM_GMT.dayOfWeek(londonSecondsFromGMT), "Wed")
+        XCTAssertEqual(oct_25_08AM_GMT.dayOfWeek(tokyoSecondsFromGMT), "Wed")
+        XCTAssertEqual(oct_25_08AM_GMT.dayOfWeek(honoluluSecondsFromGMT), "Tue")
 
-        let dateOct_25_08PM_GMT = Date(timeIntervalSince1970: 1698264000)
-        XCTAssertEqual(dateOct_25_08PM_GMT.shortDayOfWeek(londonSecondsFromGMT), "Wed")
-        XCTAssertEqual(dateOct_25_08PM_GMT.shortDayOfWeek(tokyoSecondsFromGMT), "Thu")
-        XCTAssertEqual(dateOct_25_08PM_GMT.shortDayOfWeek(honoluluSecondsFromGMT), "Wed")
+        let oct_25_08PM_GMT = Date(timeIntervalSince1970: 1698264000)
+        XCTAssertEqual(oct_25_08PM_GMT.dayOfWeek(londonSecondsFromGMT), "Wed")
+        XCTAssertEqual(oct_25_08PM_GMT.dayOfWeek(tokyoSecondsFromGMT), "Thu")
+        XCTAssertEqual(oct_25_08PM_GMT.dayOfWeek(honoluluSecondsFromGMT), "Wed")
+
+        XCTAssertEqual(Date().dayOfWeek(0), "Today")
     }
 
     func testFormattedTime() {
         continueAfterFailure = false
-        let dateOct_25_08AM_GMT = Date(timeIntervalSince1970: 1698220800)
-        XCTAssertEqual(dateOct_25_08AM_GMT.formattedTime(londonSecondsFromGMT), "09:00")
-        XCTAssertEqual(dateOct_25_08AM_GMT.formattedTime(tokyoSecondsFromGMT), "17:00")
-        XCTAssertEqual(dateOct_25_08AM_GMT.formattedTime(honoluluSecondsFromGMT), "22:00")
+        let oct_25_08AM_GMT = Date(timeIntervalSince1970: 1698220800)
+        XCTAssertEqual(oct_25_08AM_GMT.formattedTime(londonSecondsFromGMT), "09:00")
+        XCTAssertEqual(oct_25_08AM_GMT.formattedTime(tokyoSecondsFromGMT), "17:00")
+        XCTAssertEqual(oct_25_08AM_GMT.formattedTime(honoluluSecondsFromGMT), "22:00")
 
-        let dateOct_25_08PM_GMT = Date(timeIntervalSince1970: 1698264000)
-        XCTAssertEqual(dateOct_25_08PM_GMT.formattedTime(londonSecondsFromGMT), "21:00")
-        XCTAssertEqual(dateOct_25_08PM_GMT.formattedTime(tokyoSecondsFromGMT), "05:00")
-        XCTAssertEqual(dateOct_25_08PM_GMT.formattedTime(honoluluSecondsFromGMT), "10:00")
+        let oct_25_08PM_GMT = Date(timeIntervalSince1970: 1698264000)
+        XCTAssertEqual(oct_25_08PM_GMT.formattedTime(londonSecondsFromGMT), "21:00")
+        XCTAssertEqual(oct_25_08PM_GMT.formattedTime(tokyoSecondsFromGMT), "05:00")
+        XCTAssertEqual(oct_25_08PM_GMT.formattedTime(honoluluSecondsFromGMT), "10:00")
     }
 
     private static func secondsIn(hours: Int) -> Int {
